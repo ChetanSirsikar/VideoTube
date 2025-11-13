52c423d3585e14cb3753b554b8b3e0cdebe103a4
Chetan <chetansirsikar2005@gmail.com>
2025-11-14 00:21:57 +0530

    util codes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
-import dotenv from 'dotenv'
-import connectDB from './db/index.js'
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import express from "express";
+
+const app = express();
 dotenv.config({
-    path : './env'
-})
+  path: "./env",
+});
 
 connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("error", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("server running on port", process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log("mongo db connection failed", err);
+  });
