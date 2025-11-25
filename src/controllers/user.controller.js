edda1a33592759afaaf7bbde690b25c9ac7545d5
Chetan <chetansirsikar2005@gmail.com>
2025-11-25 22:08:37 +0530

    add new routes to user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -407,6 +407,8 @@ const getWatchHistory = asyncHandler(async (req,res)=>{
     .json(new ApiResponse(200,user[0].watchHistory, "Watch history fetched successfully"))
 })
 
+
+
 export {
         registerUser,
         loginUser,
